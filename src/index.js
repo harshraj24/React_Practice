790f252834af622df00b0dbc33158d70fa3310fe
Harsh Raj <me@harshraj.xyz>
2020-11-12 23:49:24 +0530

    Changes in index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as styles from './index.css';
+
 
 ReactDOM.render(
-  <>
-    <h1> Harsh Netflix Pack </h1>
-    <p> List of 5 best Series</p>
-    <ol>
-      <li>Stranger Things</li>
-      <li>Witcher</li>
-      <li>Sherlock Homes</li>
-      <li>Sacred Games</li>
-      <li>Dark</li>
-    </ol>
-  </>,
+  
+  <React.StrictMode>
+    <h1 className={styles.mystyle}>Hello World !</h1>;
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
