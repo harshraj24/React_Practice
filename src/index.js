730f04a98a5131476896698d9fc9b784d4550f91
Harsh Raj <me@harshraj.xyz>
2020-11-17 23:57:35 +0530

    Added styles

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ ReactDOM.render(
   <React.StrictMode>
     <h1 className={styles.mystyle}>Hi!</h1>
     <h1 style="padding:10px;margin:10px;">This is Harsh Raj.</h1>
-    <p>A Front-End Developer</p>
+    <p style="padding:10px;margin:10px;">A Front-End Developer</p>
     <p>A Final Year Student @ Asansol Engineering College</p>
     <App/>
   </React.StrictMode>,
