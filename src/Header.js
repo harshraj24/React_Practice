7785ef96f3873507e7e352a9c04de0d334ae1036
Harsh Raj <me@harshraj.xyz>
2021-01-04 00:33:42 +0530

    Change in Header.js for cloning Tinder

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import './Header.css'
+import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
+import logo from './logo.png';
+import ForumIcon from '@material-ui/icons/Forum';
+
 
 function Header() {
     return (
         <div className="header">
-            <h2>Header is working Properly.</h2>
+        <IconButton>
+            <PersonIcon fontSize="large" className="headerIcon"/>
+        </IconButton>
+        <IconButton>
+             <img src={logo} alt="" className='headerLogo' />
+        </IconButton>
+        <IconButton>
+            <ForumIcon/>
+        </IconButton>
         </div>
     )
 }
