419a508731b92168e39743500bbc897e9feb00a7
Harsh Raj <me@harshraj.xyz>
2021-01-03 23:58:49 +0530

    Added a new component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header.js'
 
 function App() {
   return (
     <div className="App">
       <h1>Hi. This is Harsh Raj.</h1>
+      {/* <Header> */}
+      <Header />
     </div>
   );
 }
