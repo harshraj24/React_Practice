29015df8d849c8367feeed00a9b4a7ceba909086
Harsh Raj <me@harshraj.xyz>
2021-01-12 22:42:45 +0530

    Change in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header.js'
+import TinderCards from './TinderCards.js'
 
 function App() {
   return (
     <div className="App">
       {/* <Header> */}
       <Header />
+      <TinderCards />
     </div>
   );
 }
